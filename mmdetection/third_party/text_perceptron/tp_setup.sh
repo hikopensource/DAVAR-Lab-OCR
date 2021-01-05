79776915c616731698d452d935e7b599b1ce46f0
chengzhanzhan <chengzhanzhan@hikvision.com>
2021-01-05 16:26:28 +0800

    Add cpp src

--- mmdetection/third_party/text_perceptron/tp_setup.sh
+++ mmdetection/third_party/text_perceptron/tp_setup.sh
@@ -1,3 +1,7 @@
-g++ -shared -o ./mmdet/datasets/pipelines/lib/tp_data.so -fPIC mmdet/datasets/pipelines/lib/tp_data.cpp `pkg-config --cflags --libs opencv`
+mkdir ./mmdet/datasets/pipelines/lib/
 
-g++ -shared -o ./mmdet/models/shape_transform_module/lib/tp_points_generate.so -fPIC mmdet/models/shape_transform_module/lib/tp_points_generate.cpp `pkg-config --cflags --libs opencv`
+g++ -shared -o ./mmdet/datasets/pipelines/lib/tp_data.so -fPIC mmdet/datasets/pipelines/src/tp_data.cpp `pkg-config --cflags --libs opencv`
+
+mkdir ./mmdet/models/shape_transform_module/lib/
+
+g++ -shared -o ./mmdet/models/shape_transform_module/lib/tp_points_generate.so -fPIC mmdet/models/shape_transform_module/src/tp_points_generate.cpp `pkg-config --cflags --libs opencv`
