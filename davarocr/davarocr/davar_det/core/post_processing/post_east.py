c65285f6668864cca7a12770ae4c8d083ea1cf1b
qiaoliang <qiaoliang6@hikvision.com>
2021-10-09 19:33:57 +0800

    rm multiprocess for east

--- davarocr/davarocr/davar_det/core/post_processing/post_east.py
+++ davarocr/davarocr/davar_det/core/post_processing/post_east.py
@@ -17,67 +17,6 @@ import numpy.ctypeslib as ctl
 from davarocr.davar_common.core import POSTPROCESS
 from .post_detector_base import BasePostDetector
 
-
-# Load ctl lib as global variable for multi-process
-cur_path = os.path.realpath(__file__)  # Current file path
-lib_dir = cur_path.replace('\\', '/').split('/')[:-1]
-lib_dir = "/".join(lib_dir) + '/lib'
-lib_name = "east_postprocess.so"
-lib = ctl.load_library(lib_name, lib_dir)
-generate_func = lib.generate_result
-generate_func.argtypes = [ctl.ndpointer(np.float32, flags='C_CONTIGUOUS'),  # score map
-                          ctl.ndpointer(np.float32, flags='C_CONTIGUOUS'),  # geo map
-                          c_int,                                              # height
-                          c_int,                                              # width
-                          c_int,                                              # pool ratio
-                          c_float,                                            # scale factor
-                          c_float,                                            # thres_text
-                          c_float,                                            # nms_thres
-                          c_int,                                              # nms_method
-                          ctl.ndpointer(np.float32, flags='C_CONTIGUOUS'),  # result
-                          POINTER(c_int)                                      # result_num
-                          ]
-
-
-def post_east(cur_score_map,
-              cur_geo_map,
-              height,
-              width,
-              pool_ratio,
-              scale_factor,
-              thres_text,
-              nms_thres,
-              nms_method,
-              ):
-    """ Calling ctl lib to do post processing
-
-    Args:
-        cur_score_map(np.ndarray): predict score map, in shape of 1D with length of [Bx1xHxW]
-        cur_geo_map(np.ndarray): predict geo map, in shape 1D with length of [Bx5xHxW] or [Bx8xHxW]
-        height(int): feature map height, H
-        width(int): feature map width, W
-        pool_ratio(int): feature map ratio, defaltu as 4
-        scale_factor(float):  ratio of original feature map to original image
-        thres_text(float): threshold to detect text
-        nms_thres(float): iou threshold when conduct nms
-        nms_method(int): nms mode, 0: standard RBOX, 1: long-text RBOX, 2: standard QUAD, 2: long-text QUAD.
-                         long-text mode means pixels only predict their nearest corner.
-        class_num(int): segmentation classes, default as 1.
-
-    Returns:
-        np.ndarray: predict results, in shape of [N, 9], box coordinates and confidence.
-    Returns:
-        int: result counts.
-    """
-
-    result = np.zeros((width * height, 9), dtype=np.float32)  # 8 for coordinates and 1 for confidence
-    result_num = c_int()
-
-    generate_func(cur_score_map.reshape(-1), cur_geo_map.reshape(-1), height, width, pool_ratio, scale_factor,
-                  thres_text, nms_thres, nms_method, result, result_num)
-    return result, result_num.value
-
-
 @POSTPROCESS.register_module()
 class PostEAST(BasePostDetector):
     """ EAST post-processing, using geo map and score map to generate final boxes."""
@@ -86,6 +25,8 @@ class PostEAST(BasePostDetector):
                  nms_thres=0.2,
                  nms_method="RBOX",
                  num_workers=0,
+                 lib_name=None,
+                 lib_dir="./",
                  ):
         """
         Args:
@@ -109,11 +50,69 @@ class PostEAST(BasePostDetector):
             self.pool = multiprocessing.Pool(num_workers)
         self.count = 1
 
-    def __del__(self):
-        """ Multiprocessing free"""
-        if self.num_workers > 0:
-            self.pool.close()
-            self.pool.join()
+        if lib_name is None or not os.path.isfile(os.path.join(lib_dir, lib_name)):
+            cur_path = os.path.realpath(__file__)
+            lib_dir = cur_path.replace('\\', '/').split('/')[:-1]
+            lib_dir = "/".join(lib_dir) + '/lib'
+            lib_name = "east_postprocess.so"
+
+        lib = ctl.load_library(lib_name, lib_dir)
+        self.generate_func = lib.generate_result
+        self.generate_func.argtypes = [
+            ctl.ndpointer(np.float32, flags='C_CONTIGUOUS'),  # score map
+            ctl.ndpointer(np.float32, flags='C_CONTIGUOUS'),  # geo map
+            c_int,  # height
+            c_int,  # width
+            c_int,  # pool ratio
+            c_float,  # scale factor
+            c_float,  # thres_text
+            c_float,  # nms_thres
+            c_int,  # nms_method
+            ctl.ndpointer(np.float32, flags='C_CONTIGUOUS'),  # result
+            POINTER(c_int)  # result_num
+            ]
+
+    def post_east(self,
+                  cur_score_map,
+                  cur_geo_map,
+                  height,
+                  width,
+                  pool_ratio,
+                  scale_factor,
+                  thres_text,
+                  nms_thres,
+                  nms_method,
+                  ):
+        """ Calling ctl lib to do post processing
+
+        Args:
+            cur_score_map(np.ndarray): predict score map, in shape of 1D with length of [Bx1xHxW]
+            cur_geo_map(np.ndarray): predict geo map, in shape 1D with length of [Bx5xHxW] or [Bx8xHxW]
+            height(int): feature map height, H
+            width(int): feature map width, W
+            pool_ratio(int): feature map ratio, defaltu as 4
+            scale_factor(float):  ratio of original feature map to original image
+            thres_text(float): threshold to detect text
+            nms_thres(float): iou threshold when conduct nms
+            nms_method(int): nms mode, 0: standard RBOX, 1: long-text RBOX, 2: standard QUAD, 2: long-text QUAD.
+                             long-text mode means pixels only predict their nearest corner.
+            class_num(int): segmentation classes, default as 1.
+
+        Returns:
+            np.ndarray: predict results, in shape of [N, 9], box coordinates and confidence.
+        Returns:
+            int: result counts.
+        """
+
+        result = np.zeros((width * height, 9),
+                          dtype=np.float32)  # 8 for coordinates and 1 for confidence
+        result_num = c_int()
+
+        self.generate_func(cur_score_map.reshape(-1), cur_geo_map.reshape(-1),
+                      height, width, pool_ratio, scale_factor,
+                      thres_text, nms_thres, nms_method, result, result_num)
+        return result, result_num.value
+
 
     def post_processing(self, results, img_meta):
         """
@@ -145,16 +144,14 @@ class PostEAST(BasePostDetector):
                         scale_factor = (img_meta[i]['scale_factor'][0] + img_meta[i]['scale_factor'][1]) / 2
                     else:
                         scale_factor = float(img_meta[i]['scale_factor'])
-                res = self.pool.apply_async(
-                    func=post_east,
-                    args=(cur_score_map.reshape(-1),
-                          cur_geo_map.reshape(-1),
-                          height, width, 4,
-                          scale_factor,
-                          self.thres_text,
-                          self.nms_thres,
-                          self.nms_method,
-                          ))
+
+                res = self.post_east(cur_score_map.reshape(-1),
+                                     cur_geo_map.reshape(-1),
+                                     height, width, 4,
+                                     scale_factor,
+                                     self.thres_text,
+                                     self.nms_thres,
+                                     self.nms_method)
                 res_list.append(res)
             res_list = [r.get() for r in res_list]
         else:
