57ff7aec0ffe50eeaa0c206bf4cce3ed16d66e8e
qiaoliang <qiaoliang6@hikvision.com>
2022-09-05 15:20:41 +0800

    add online evaluation for lgpma

--- demo/table_recognition/lgpma/dist_train.sh
+++ demo/table_recognition/lgpma/dist_train.sh
@@ -4,4 +4,4 @@ export LANGUAGE=zh_CN:zh:en_US:en
 export PATH=/usr/local/miniconda3/bin/:$PATH
 
 DAVAROCR_PATH=/path/to/Davar-Lab-OCR/
-python -m torch.distributed.launch --nproc_per_node 8 $DAVAROCR_PATH/tools/train.py  ./config/lgpma_pub.py --no-validate --launcher pytorch
+python -m torch.distributed.launch --nproc_per_node 8 $DAVAROCR_PATH/tools/train.py  ./configs/lgpma_pub.py --no-validate --launcher pytorch
